958d5882eed69d9ddc894f0b4df292b295e7e7f0
Dora <dora.sparagno@gmail.com>
2024-01-30 16:25:42 +0100

    modified createObj and added og image

--- src/js/createObj.js
+++ src/js/createObj.js
@@ -3,24 +3,27 @@ const { default: _, forEach } = require("lodash");
 
 //reates an array of objects containing book details for a given category
 async function createObj(category) {
-  const response = await axios.get(
-    "https://openlibrary.org/subjects/" + category + ".json"
-  );
-  const obj = response.data.works;
-  const objArray = [];
-  obj.forEach((element) => {
-    let newObj = {};
-    newObj["key"] = element.key;
-    newObj["title"] = element.title;
-    newObj["author"] = element.authors
-      .map(function (author) {
-        return author.name;
-      })
-      .join(", ");
-    newObj["cover"] = element.cover_edition_key;
-    objArray.push(newObj);
-  });
-  return objArray;
+  try {
+    const response = await axios.get(
+      "https://openlibrary.org/subjects/" + category + ".json"
+    );
+    const obj = response.data.works;
+    const objArray = [];
+    obj.forEach((element) => {
+      let newObj = {};
+      newObj["key"] = element.key;
+      newObj["title"] = element.title;
+      newObj["author"] = element.authors
+        .map(function (author) {
+          return author.name;
+        })
+        .join(", ");
+      newObj["cover"] = element.cover_edition_key;
+      objArray.push(newObj);
+    });
+    return objArray;
+  } catch (error) {
+    console.log("Error in creating object", error);
+  }
 }
-
 module.exports = createObj;
